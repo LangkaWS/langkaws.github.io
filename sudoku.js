1793b2739aed8c3b7888f9c81b751ea33f582dde
Langka <60548795+LangkaWS@users.noreply.github.com>
2020-07-26 10:38:32 +0200

    Update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -1,19 +1,18 @@
 function validSolution(board){
-  let columns = [[],[],[],[],[],[],[],[],[]], grid = [[],[],[],[],[],[],[],[],[]];
-  let row = 0, col = 0, gridIndex = 0;
-  for(row = 0; row < 9; row++) {
-    for(col = 0; col < 9; col++) {
+  let columns = [[],[],[],[],[],[],[],[],[]], grid = [[],[],[],[],[],[],[],[],[]], gridIndex = 0;
+  for(let row = 0; row < board.length; row++) {
+    for(let col = 0; col < board.length; col++) {
       gridIndex = Math.floor(row/3)*3 + Math.floor(col/3);
       grid[gridIndex].push(board[row][col]);
       columns[col].push(board[row][col]);
     }
   }
-  for(let i = 1; i <= 9; i++) {
-    for(let j = 0; j < 9; j++) {
-      if(board[j].filter(el => el == i).length !== 1 || columns[j].filter(el => el ==i).length !== 1 || grid[j].filter(el => el == i).length !== 1) {
-        return false;
+  for (let digit = 1; digit <= 9; digit++) {
+    for(let i = 0; i < board.length; i++) {
+      if (board[i].filter(el => el === digit).length !== 1 || columns[i].filter(el => el === digit).length !== 1 || grid[i].filter(el => el === digit).length !== 1) {
+        return true;
       }
     }
   }
-  return true;
+  return false;
 }
